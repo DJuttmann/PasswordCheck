72969a6bebab6d3503f28ffce3bb324fe46879ee
NULL <daan_jutt@hotmail.com>
2018-04-03 12:47:38 +0200

    Add project files.

---
+++ PasswordCheck/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+
+namespace PasswordCheck
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow: Window
+  {
+
+    BindingList <PasswordCounter> Passwords;
+    
+
+    public MainWindow ()
+    {
+      InitializeComponent ();
+
+      Passwords = new BindingList <PasswordCounter> ();
+      CheckedPasswords.ItemsSource = Passwords;
+    }
+
+
+    private void AddPassword (PasswordCounter p)
+    {
+      Passwords.Add (p);
+    }
+
+
+    private void UpdatePassword (PasswordCounter p, string count)
+    {
+      p.Count = count;
+      Passwords.ResetItem (Passwords.IndexOf (p));
+    }
+
+
+    private async Task <bool> CheckPassword ()
+    {
+      string password = PasswordInput.Text;
+      PasswordInput.Text = String.Empty;
+
+      PasswordCounter counter = new PasswordCounter ()
+      {
+        Password = password,
+        Count = String.Empty
+      };
+      await CheckedPasswords.Dispatcher.BeginInvoke (new Action (() => AddPassword (counter)));
+
+      string count = await PasswordChecker.PasswordCount (password);
+      await CheckedPasswords.Dispatcher.BeginInvoke (
+        new Action (() => UpdatePassword (counter, count)));
+      return true;
+    }
+
+
+    private async void Button_Click (object sender, RoutedEventArgs e)
+    {
+      await CheckPassword ();
+    }
+
+
+    private async void PasswordInput_KeyDown (object sender, KeyEventArgs e)
+    {
+      if (e.Key == Key.Enter)
+        await CheckPassword ();
+    }
+
+
+    private void RemovePassword_Click (object sender, RoutedEventArgs e)
+    {
+      Passwords.Remove (((Button) sender).Tag as PasswordCounter);
+    }
+  }
+}
---
+++ PasswordCheck/PasswordChecker.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace PasswordCheck
+{
+
+  // Saves a password and the number of occurrences in the database.
+  class PasswordCounter
+  {
+    public string Password {get; set;}
+    public string Count {get; set;}
+  }
+
+
+  // Provides method for checking the database for given passwords.
+  class PasswordChecker
+  {
+    private const string baseUrl = "https://api.pwnedpasswords.com/pwnedpassword/";
+
+
+    public static async Task <string> PasswordCount (string password)
+    {
+      HttpWebRequest request = WebRequest.CreateHttp (baseUrl + password);
+      HttpWebResponse response;
+
+      try {
+        response = await request.GetResponseAsync () as HttpWebResponse;
+      }
+      catch (WebException ex)
+      {
+        if (ex.Status == WebExceptionStatus.ProtocolError)
+        {
+          response = ex.Response as HttpWebResponse;
+        }
+        else return "Failed";
+      }
+
+      switch (response.StatusCode)
+      {
+      case HttpStatusCode.OK:
+        Stream s = response.GetResponseStream ();
+        StreamReader reader = new StreamReader (s);
+        String value = reader.ReadLine ();
+        Int32.TryParse (value, out int result);
+        return result.ToString () + "\u00d7";
+      case HttpStatusCode.NotFound:
+        return "0\u00d7";
+      case (HttpStatusCode) 429: // Too many requests
+        return "Too Many Requests";
+      default:
+        return "Error " + (int) response.StatusCode;
+      }
+    }
+
+  } // class PasswordChecker
+
+}
